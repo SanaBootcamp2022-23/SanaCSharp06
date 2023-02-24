82bc865f0d4704060ddd00fa38daff04051f78e5
Vadim Kuzmuk <ipzm221_kvo@student.ztu.edu.ua>
2023-02-24 16:50:04 +0200

    add Class Person

---
+++ SanaCSharp06/ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+using ClassLibrary;
+Console.OutputEncoding = Encoding.Unicode;
+Console.InputEncoding = Encoding.Unicode;
+
+List<Person> persons = new List<Person>()
+{
+    new Person("Вадим", "Кузьмук", new DateTime(2000, 07, 08)),
+    new Person("Вадим", "Кузьмук")
+};
+
+foreach (var person in persons)
+{
+    person.ShowInfo();
+    Console.WriteLine(new string('-', 25));
+}
