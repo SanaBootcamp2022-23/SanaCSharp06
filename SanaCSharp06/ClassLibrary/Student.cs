e2a6d610ebd725faee49408c74b0ccc40697bfd0
Vadim Kuzmuk <ipzm221_kvo@student.ztu.edu.ua>
2023-02-24 17:26:11 +0200

    add Class Student

---
+++ SanaCSharp06/ClassLibrary/Student.cs
@@ -0,0 +1,45 @@
+﻿namespace ClassLibrary
+{
+    public class Student : Person
+    {
+        public uint YearOfStudy { get; set; }
+        public string GroupName { get; set; }
+        public string FacultyName { get; set; }
+        public string UniversityName { get; set; }
+        public Student(string firstname, string lastname, DateTime birthDate, 
+            uint yearOfStudy, string groupName, string facultyName, string universityName) : 
+            base(firstname, lastname, birthDate)
+        {
+            YearOfStudy = yearOfStudy;
+            GroupName = groupName;
+            FacultyName = facultyName;
+            UniversityName = universityName;
+        }
+        public Student(string firstname, string lastname, DateTime birthDate, 
+            uint yearOfStudy, string groupName, string facultyName) : 
+            base(firstname, lastname, birthDate)
+        {
+            YearOfStudy = yearOfStudy;
+            GroupName = groupName;
+            FacultyName = facultyName;
+        }
+        public Student(string firstname, string lastname, DateTime birthDate,
+            string groupName, string facultyName, string universityName) :
+            base(firstname, lastname, birthDate)
+        {
+            GroupName = groupName;
+            FacultyName = facultyName;
+            UniversityName = universityName;
+        }
+
+        public override void ShowInfo()
+        {
+            base.ShowInfo();
+            string information = $"Курс: {YearOfStudy};" +
+                $"\nГрупа: {GroupName};" +
+                $"\nФакультет: {FacultyName};" +
+                $"\nВищий навчальний заклад: {UniversityName};";
+            Console.WriteLine(information);
+        }
+    }
+}
--- SanaCSharp06/ConsoleApp/Program.cs
+++ SanaCSharp06/ConsoleApp/Program.cs
@@ -7,7 +7,8 @@ List<Person> persons = new List<Person>()
 {
     new Person("Вадим", "Кузьмук", new DateTime(2000, 07, 08)),
     new Person("Вадим", "Кузьмук"),
-    new Entrant("Вадим", "Кузьмук", new DateTime(2001, 08, 09), 190, 10, "Назва школи")
+    new Entrant("Вадим", "Кузьмук", new DateTime(2001, 08, 09), 190, 10, "Назва школи"),
+    new Student("Вадим", "Кузьмук", new DateTime(2002, 09, 10), 1, "IPZm-22-1", "FICT", "ZTU"),
 };
 
 foreach (var person in persons)
