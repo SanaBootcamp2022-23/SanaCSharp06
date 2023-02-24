1ca32a587d93e4e73baa793a1e11915719cef8a2
Kuprienko Anatolii <ipz222_kaa@student.ztu.edu.ua>
2023-02-24 12:56:48 +0200

    Class Abiturient Created

--- SanaCSharp06/ClassLibrary/Class1.cs
+++ SanaCSharp06/ClassLibrary/Class1.cs
@@ -8,20 +8,20 @@
         protected string BirthDate;
         public Person()
         {
-            this.FirstName = "John";
-            this.LastName = "Smith";
-            this.BirthDate = "Unknown";
+            FirstName = "John";
+            LastName = "Smith";
+            BirthDate = "Unknown";
         }
         public Person(string firstName, string lastName, uint birthDay, uint birthMonth, uint birthYear)
         {
-            this.SetFirstName(firstName);
-            this.SetLastName(lastName);
-            this.SetBirthDate(birthDay, birthMonth, birthYear);
+            SetFirstName(firstName);
+            SetLastName(lastName);
+            SetBirthDate(birthDay, birthMonth, birthYear);
         }
         public Person(string lastName, string firstName)
         {
-            this.SetFirstName(firstName);
-            this.SetLastName(lastName);
+            SetFirstName(firstName);
+            SetLastName(lastName);
             BirthDate = "Unknown";
         }
         public string GetFirstName()
@@ -60,8 +60,20 @@
                 {
                     if (birthMonth == 2)
                     {
-                        if (birthDay <= 29)
+
+                        if (birthYear % 4 == 0)
+                        {
+                            if (birthDay <= 29)
+                            {
+                                this.BirthDate = $"{birthDay}.{birthMonth}.{birthYear}";
+                            }
+                            else throw new Exception("Invalid Day value");
+                        }else if (birthDay <= 28)
+                        {
                             this.BirthDate = $"{birthDay}.{birthMonth}.{birthYear}";
+                        }
+                        else throw new Exception("Invalid Day value");
+
                     }
                     else if (birthMonth % 2 == 1 || birthMonth == 8)
                     {
@@ -69,11 +81,15 @@
                         {
                             this.BirthDate = $"{birthDay}.{birthMonth}.{birthYear}";
                         }
+                        else
+                            throw new Exception("Invalid Day value");
                     }
                     else if (birthDay <= 30)
                     {
                         this.BirthDate = $"{birthDay}.{birthMonth}.{birthYear}";
                     }
+                    else
+                        throw new Exception("Invalid Day value");
                 }
                 else if (birthMonth % 2 == 0)
                 {
@@ -81,16 +97,92 @@
                     {
                         this.BirthDate = $"{birthDay}.{birthMonth}.{birthYear}";
                     }
+                    else
+                        throw new Exception("Invalid Day value");
                 }
                 else if (birthDay <= 30)
                 {
                     this.BirthDate = $"{birthDay}.{birthMonth}.{birthYear}";
                 }
+                else throw new Exception("Invalid Month Value");
             }
+            else throw new Exception("Invalid Year Value");
         }
         public virtual string ShowInfo()
         {
             return $"Name:{LastName} {FirstName} Born:{BirthDate}";
+        }
+    }
+    public class Abiturient : Person
+    {
+        protected uint znoResult;
+        protected double basicEducationResult;
+        protected string schoolName;
+        public Abiturient()
+        {
+            FirstName = "John";
+            LastName = "Smith";
+            BirthDate = "Unknown";
+            znoResult = 0;
+            basicEducationResult = 0;
+            schoolName = "Unknown";
+        }
+        public Abiturient(string lastName, string firstName, uint birthDay, uint birthMonth,
+            uint birthYear, uint znoResult, uint basicEducationResult, string schoolName) :
+            base(firstName, lastName, birthDay, birthMonth, birthYear)
+        {
+            SetZNOResult(znoResult);
+            SetSchoolName(schoolName);
+            SetBasicEducationResult(basicEducationResult);
+        }
+        public Abiturient(string lastName, string firstName, uint znoResult, 
+            double basicEducationResult, string schoolName):base(lastName, firstName)
+        {
+            SetZNOResult(znoResult);
+            SetSchoolName(schoolName);
+            SetBasicEducationResult(basicEducationResult);
+        }
+
+        public uint GetZNOResult()
+        {
+            return znoResult;
+        }
+        public double GetBasicEducationResult()
+        {
+            return basicEducationResult;
+        }
+        public string GetSchoolName()
+        {
+            return $"{schoolName}";
+        }
+        public void SetZNOResult(uint znoResult)
+        {
+            if (znoResult <= 200)
+            {
+                this.znoResult = znoResult;
+            }
+            else throw new Exception("Invalid ZNO Result value");
+        }
+        public void SetBasicEducationResult(double basicEducationResult)
+        {
+            if (basicEducationResult > 0 && basicEducationResult <= 12)
+            {
+                this.basicEducationResult = basicEducationResult;
+            }
+            else throw new Exception("Invalid Basic Education Result value");
+        }
+        public void SetSchoolName(string schoolName)
+        {
+            if (schoolName != null)
+            {
+                this.schoolName = schoolName;
+            }
+            else throw new Exception("Invalid School Name value");
+        }
+        public override string ShowInfo()
+        {
+            return $"{base.ShowInfo()}\nZNO Results: {znoResult}, School Results: {basicEducationResult}, " +
+                $"School Name: {schoolName}";
         }
     }
 }
--- SanaCSharp06/OOP2/Program.cs
+++ SanaCSharp06/OOP2/Program.cs
@@ -1,7 +1,4 @@
 ﻿using OOP2;
 
-Person person = new Person();
-person.SetLastName("Ivanenko");
-person.SetFirstName("Ivan");
-person.SetBirthDate(14, 5, 2000);
-Console.Write(person.ShowInfo());
+Abiturient Abiturient= new Abiturient();
+Console.WriteLine(Abiturient.ShowInfo());
