94bd425281e916e26a32e18e22c2d2a58f72f862
Yaroslav Mykhalchuk <ipzm222_myaa@student.ztu.edu.ua>
2023-02-26 00:58:29 +0200

    Add Person.cs

---
+++ University/Person.cs
@@ -0,0 +1,41 @@
+﻿namespace University
+{
+    public class Person
+    {
+        public string Name
+        {
+            get { return Name; }
+            set
+            {
+                if (!String.IsNullOrEmpty(Name))
+                    Name = value;
+            }
+        }
+        public string Surname
+        {
+            get { return Surname; }
+            set
+            {
+                if (!String.IsNullOrEmpty(Surname))
+                    Surname = value;
+            }
+        }
+        public DateTime DateOfBirth { get; set; }
+
+        public Person() { }
+        public Person(string Name, string Surname, DateTime DateOfBirth)
+        {
+            this.Name = Name;
+            this.Surname = Surname;
+            this.DateOfBirth = DateOfBirth;
+        }
+        public Person(string Name, string Surname) : this(Name, Surname, new DateTime())
+        { }
+
+        public virtual void ShowInfo()
+        {
+            Console.WriteLine($"Name: {Name}\nSurname: {Surname}\n" +
+                              $"Date of birth: {DateOfBirth.ToShortDateString()}\n");
+        }
+    }
+}
