aeb3eedb01b93e8960a2221e15eddc4d2ff7eb34
Vyacheslav Smolianiuk <sana22_svv@student.ztu.edu.ua>
2023-02-25 21:08:15 +0200

    Complete

---
+++ SanaCSharp6/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SanaCSharp6
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string Birth { get; set; }
+
+        public Person(){}
+
+        public Person(string name,string surName,string birth) {
+            Name=name;
+            SurName=surName;
+            Birth = birth;
+        }
+        public Person(string name, string surName)
+        {
+            Name = name;
+            SurName = surName;
+            
+        }
+        public virtual void ShowInfo()
+        {
+            string info = $"Ім'я: {Name};\nПрізвище: {SurName};";
+            info += Birth == "" ? "" : $"\nДата народження: {Birth}";
+            Console.WriteLine(info);
+        }
+    }
+}
---
+++ SanaCSharp6/Teacher.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace SanaCSharp6
+{
+    public class Teacher : Person
+    {
+        public string Position { get; set; }
+        public string Chair { get; set; }
+        public string UniversityName
+        {
+            get; set;
+        }
+        public Teacher(string name, string surname, string dateOfBirth, string position, string chair, string universityNAme) : base(name, surname, dateOfBirth)
+        {
+            Position = position;
+            Chair = chair;
+            UniversityName = universityNAme;
+        }
+        public Teacher(string name, string surname, string dateOfBirth, string position, string chair) : base(name, surname, dateOfBirth)
+        {
+            Position = position;
+            Chair = chair;
+
+        }
+        public override void ShowInfo()
+        {
+            base.ShowInfo();
+            string info = $"Посада: {Position};\nКафедра: {Chair};";
+            info += UniversityName == "" ? "" : $"\nНазва вищого навчального закладу: {UniversityName}";
+            Console.WriteLine(info);
+        }
+    }
+}
