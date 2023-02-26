6a6d72901ce0986763ba07c89acc093c28b6acf3
Ivan Plias <sana22_pii@student.ztu.edu.ua>
2023-02-26 15:37:37 +0200

    Class Student added

---
+++ SanaCSharp06/People/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace People
+{
+    public class Student : Person
+    {
+        public int Course { get; set; }
+        public string Group { get; set; }
+        public string Faculty { get; set; }
+        public string UniversityName { get; set; }
+        public Student()
+        {
+            Course = 0;
+            Group = String.Empty;
+            Faculty = String.Empty;
+            UniversityName = String.Empty;
+        }
+        public Student(string Name, string SurName, int Course, string UniversityName) : base(Name, SurName)
+        {
+            this.Course = Course;
+            Group = String.Empty;
+            Faculty = String.Empty;
+            this.UniversityName = UniversityName;
+        }
+        public Student(string Name, string SurName, DateTime Birthday, int Course, string Group, string Faculty, string UniversityName) : base(Name, SurName, Birthday)
+        {
+            this.Course = Course;
+            this.Group = Group;
+            this.Faculty = Faculty;
+            this.UniversityName = UniversityName;
+        }
+        public override void ShowInfo()
+        {
+            base.ShowInfo();
+            Console.WriteLine($"Course: {Course}\nGroup: {Group}\nFaculty: {Faculty}\nName of University: {UniversityName}");
+        }
+    }
+}
