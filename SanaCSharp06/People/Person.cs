be24d6d79214763830a534a670720339e934f16e
Ivan Plias <sana22_pii@student.ztu.edu.ua>
2023-02-26 15:35:35 +0200

    Class Person added

---
+++ SanaCSharp06/People/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace People
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public DateTime Birthday { get; set; }
+        public Person()
+        {
+            Name= string.Empty;
+            SurName= string.Empty;
+            Birthday = DateTime.MinValue;
+        }
+        public Person(string Name, string SurName)
+        {
+            this.Name=Name;
+            this.SurName= SurName;
+            Birthday = DateTime.MinValue;
+        }
+        public Person(string Name, string SurName,DateTime Birthday)
+        {
+            this.Name = Name;
+            this.SurName = SurName;
+            this.Birthday = Birthday;  
+        }
+        public virtual void ShowInfo()
+        {
+            Console.WriteLine($"Name: {Name}\nSurname: {SurName}\nDate of birth: {Birthday.ToShortDateString()}\n");
+        }
+    }
+}
