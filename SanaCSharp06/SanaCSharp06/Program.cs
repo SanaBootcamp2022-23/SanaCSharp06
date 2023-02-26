e3614dbe6723063c368e8be1d2d9a207eda8317d
DimaDemcehnko <ipz215_ddo@student.ztu.edu.ua>
2023-02-26 12:33:15 +0200

    done

--- SanaCSharp06/SanaCSharp06/Program.cs
+++ SanaCSharp06/SanaCSharp06/Program.cs
@@ -1,32 +1,21 @@
 ﻿using ClassLibrary;
-//People test
-People people= new People("Dmytro", "Demchenko", 8, 9, 2004);
 
-Console.WriteLine(people.ShowInfo());
-
-//Entrant test
+DateTime dateTime = DateTime.Now;
 Dictionary<string, double> zno = new Dictionary<string, double>();
 
 zno.Add("Math", 181);
 zno.Add("Ukrainian Language", 181);
 zno.Add("English", 184);
 
-Entrant entrant = new Entrant("Dmytro", "Demchenko", 8, 9, 2004, "Humanitarian gymnasium #1", 10.5, zno);
+var peopleList = new List<People>(5);
 
-Console.WriteLine(entrant.ShowInfo());
+peopleList.Add(new People("Dmytro", "Demchenko", 8, 9, 2004));
+peopleList.Add(new Entrant("Dmytro", "Demchenko", 8, 9, 2004, "Humanitarian gymnasium #1", 10.5, zno));
+peopleList.Add(new Student("Dmytro", "Demchenko", 8, 9, 2004, 2, "IPZ-21-5", "FICT", "ZTU"));
+peopleList.Add(new Teacher("Teacher", "Teacher", 1, 1, 1980, "Lector", "KKIK", "ZTU"));
+peopleList.Add(new LibraryMember("Dmytro", "Demchenko", 8, 9, 2004, 1, dateTime, 250M));
 
-//Student test
-Student student = new Student("Dmytro", "Demchenko", 8, 9, 2004, 2, "IPZ-21-5", "FICT", "ZTU");
-
-Console.WriteLine(student.ShowInfo());
-
-//Teacher test
-Teacher teacher = new Teacher("Teacher", "Teacher", 1, 1, 1980, "Lector", "KKIK", "ZTU");
-
-Console.WriteLine(teacher.ShowInfo());
-
-//Library member test
-DateTime dateTime = DateTime.Now;
-LibraryMember lb = new LibraryMember("Dmytro", "Demchenko", 8, 9, 2004, 1, dateTime, 250M);
-
-Console.WriteLine(lb.ShowInfo());
+foreach (var people in peopleList)
+{ 
+    Console.WriteLine(people.ShowInfo());
+}
