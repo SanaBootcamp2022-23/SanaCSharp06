b5c1eea4669ca3c9e1875c2adb30f8167933b47e
Rod_Of_Harmony <ipz222_pis@student.ztu.edu.ua>
2023-02-27 17:29:36 +0200

    task

---
+++ ClassLibrary/LibraryUser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class LibraryUser : Person
+    {
+        public int MonthContribution { get; set; }
+        public int TicketNum { get; set; }
+        public DateTime ReceiveDate { get; set; }
+        public LibraryUser(string firstname, string lastname, DateTime birhdate, int MonthContribution, int TicketNum, 
+                           DateTime RecieveDate) : base(firstname, lastname, birhdate)
+        {          
+            this.MonthContribution = MonthContribution;
+            this.TicketNum = TicketNum;
+            this.ReceiveDate = RecieveDate;
+        }
+        public LibraryUser(Person person, int MonthContribution, int TicketNum,
+                           DateTime ReceiveDate) : this(person.firstname, person.lastname, person.dateofbirth, MonthContribution, TicketNum, ReceiveDate)
+        { }
+        public override string ShowInfo()
+        {
+            return base.ShowInfo() + $"\nНомер читацького квитка: {TicketNum} " +
+            $"\nРозмір щомісячного читацького внеску: {MonthContribution} \nДата видачі: {ReceiveDate.Day}.{ReceiveDate.Month}.{ReceiveDate.Year}";
+        }
+    }
+}
---
+++ ClassLibrary/Teacher.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Teacher : Person
+    {
+        public string position { get; set; }
+        
+        public string university { get; set; }       
+        public string departament { get; set; }       
+        public Teacher(string firstname,string lastname, DateTime dateofbirth, string position, string university, 
+                       string departament) : base(firstname,lastname, dateofbirth)
+        {
+            this.position = position;
+            this.university = university;
+            this.departament = departament;
+        }
+        public Teacher(Person person, string position, string university,
+                       string departament) : this(person.firstname, person.lastname, person.dateofbirth, position, university, departament)
+        { }
+        public override string ShowInfo()
+        {
+            return base.ShowInfo() + $"\nУніверситет: {university} \nПосада: {position} \nКафедра: {departament}";
+        }
+    }
+}
