baa41923bdeff3f4e042150ece830d247e30f4ab
Dmytro Burylko <sana22_bdv@student.ztu.edu.ua>
2023-02-28 16:32:39 +0200

    everything is done

---
+++ SanaCSharp06/ClassLibrary/Professor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Professor : Human
+    {
+        protected string position;
+        protected string department;
+        protected string university;
+        public Professor()
+        {
+            position = string.Empty;
+            department = string.Empty;
+            university= string.Empty;
+        }
+        public Professor(string name, string surname, DateOnly birthdayDate, string position, string department, string university)
+            :base(name, surname, birthdayDate)
+        {
+            this.position = position;
+            this.department = department;
+            this.university= university;
+        }
+        public Professor(string position, string department, string university)
+        {
+            this.position = position;
+            this.department = department;
+            this.university = university;
+        }
+        public string Position { get; set; }
+        public string Department { get; set; }
+        public string University { get;set; }
+        public override void ShowInfo()
+        {
+            Console.WriteLine("Lecturer: ");
+            Console.WriteLine($"  First name: {this.name}");
+            Console.WriteLine($"  Last name: {this.surname}");
+            Console.WriteLine($"  Date of birth: {this.birthdayDate}");
+            Console.WriteLine($"  Position: {this.position}");
+            Console.WriteLine($"  Department: {this.department}");
+            Console.WriteLine($"  University: {this.university}");
+        }
+    }
+}
---
+++ SanaCSharp06/ClassLibrary/Student.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Student : Human
+    {
+        protected int year;
+        protected string groupCode;
+        protected string faculty;
+        protected string university;
+        public Student()
+        {
+            year = 1;
+            groupCode= string.Empty;
+            faculty = string.Empty;
+            university = string.Empty;
+        }
+        public Student(string name, string surname, DateOnly birthdayDate, int year, string groupCode, string faculty, string university)
+            :base(name, surname, birthdayDate)
+        {
+            this.year = year;
+            this.groupCode = groupCode;
+            this.faculty= faculty;
+            this.university= university;
+        }
+        public Student(int year, string groupCode, string faculty, string university)
+        {
+            this.year = year;
+            this.groupCode = groupCode;
+            this.faculty = faculty;
+            this.university = university;
+        }
+        public int Year { set; get; }
+        public string GroupCode { set; get;}
+        public string Faculty { set; get;}
+        public string University { set; get;}
+        public override void ShowInfo()
+        {
+            Console.WriteLine("Student: ");
+            Console.WriteLine($"  First name: {this.name}");
+            Console.WriteLine($"  Last name: {this.surname}");
+            Console.WriteLine($"  Date of birth: {this.birthdayDate}");
+            Console.WriteLine($"  Year of studying: {this.year}");
+            Console.WriteLine($"  Group code: {this.groupCode}");
+            Console.WriteLine($"  Faculty: {this.faculty}");
+            Console.WriteLine($"  University: {this.university}");
+        }
+    }
+}
