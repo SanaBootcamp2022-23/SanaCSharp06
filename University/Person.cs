3c85f0726197e6bb0838292cecd3f378c4818838
filippdemchenko <filipp5632@gmail.com>
2023-02-28 12:19:30 +0200

    Create Person class

---
+++ University/Person.cs
@@ -0,0 +1,28 @@
+namespace University
+{
+    public class Person {
+        private string _firstName = "Undefined";
+        private string _lastName = "Undefined";
+        private DateOnly _dateOfBirth;
+
+        public string FirstName { get => _firstName; set => _firstName = value  ?? "Undefined"; }
+        public string LastName { get => _lastName; set => _lastName = value  ?? "Undefined"; }
+        public DateOnly DateOfBirth { get => _dateOfBirth; set => _dateOfBirth = value; }
+        
+        public Person(string firstName, string lastName) {
+           this.FirstName = firstName;
+           this.LastName = lastName;
+        }
+
+        public Person(string firstName, string lastName, DateOnly dateOfBirth) : this(firstName, lastName) {
+            this.DateOfBirth = dateOfBirth;
+        }
+
+        public virtual void ShowInfo() {
+            Console.WriteLine("Person: ");
+            Console.WriteLine($"  First name - {this.FirstName}");
+            Console.WriteLine($"  Last name - {this.LastName}");
+            Console.WriteLine($"  Date of birth  - {this.DateOfBirth}");
+        }
+    }
+}
