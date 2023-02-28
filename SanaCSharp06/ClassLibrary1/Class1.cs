074f05a2942151f52dd9361619decbff7bfdac80
TarasBlyznyuk <ipz222_btr@student.ztu.edu.ua>
2023-02-28 11:05:00 +0200

    100%: робота виконана

--- SanaCSharp06/ClassLibrary1/Class1.cs
+++ SanaCSharp06/ClassLibrary1/Class1.cs
@@ -3,21 +3,18 @@
     public class Person
     {
         public string firstName { get; set; }
-        protected string fname;
         public string lastName { get; set; }
-        protected string lname;
         public Date birthDate { get; set; }
-        protected string bdate;
+        public Person(string firstName, string lastName, Date birthDate) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.birthDate = birthDate;
+        }
         public Person()
         {
             firstName = "Тарас";
             lastName = "Близнюк";
             birthDate = new Date(12, 10, 2004);
-        }
-        public Person(string firstName, string lastName, Date birthDate) {
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.birthDate = birthDate;
         }
         public virtual string ShowInfo() {
             return $"{firstName} {lastName}, {birthDate.dateToString()}";
@@ -26,7 +23,6 @@
     public class Abiturient : Person
     {
         public ExamResults certExamMark { get; set; }
-        protected ExamResults cert1;
         public double certSchoolMark {
             get {
                 return cert2;
@@ -38,7 +34,6 @@
         }
         protected double cert2;
         public string edInstName { get; set; }
-        protected string educName;
         public Abiturient(string firstName, string lastName, Date birthDate, ExamResults certExamMark, double certSchoolMark, string edInstName) : base(firstName, lastName, birthDate) {
             this.certExamMark = certExamMark;
             this.certSchoolMark = certSchoolMark;
@@ -55,6 +50,93 @@
         public override string ShowInfo()
         {
             return $"{firstName} {lastName}, {birthDate.dateToString()}. Загальноосвітний навчальний заклад  - {edInstName}. {certExamMark.resToString()}. Оцінка в шкільному атестаті - {certSchoolMark}";
+        }
+    }
+    public class Student : Person {
+        public int studCourse
+        {
+            get {
+                return course;
+            }
+            set {
+                if (value >= 1 && value <= 4)
+                    course = value;
+            }
+        }
+        protected int course;
+        public string studGroup { get; set; }
+        public string studFaculty { get; set; }
+        public string uniName { get; set; }
+        public Student(string firstName, string lastName, Date birthDate, int studCourse, string studGroup, string studFaculty, string uniName) : base(firstName, lastName, birthDate) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.birthDate = birthDate;
+            this.studCourse = studCourse;
+            this.studGroup = studGroup;
+            this.studFaculty = studFaculty;
+            this.uniName = uniName;
+        }
+        public Student()
+        {
+            firstName = "Денис";
+            lastName = "Балюк";
+            birthDate = new Date(15, 11, 2004);
+            studCourse = 2;
+            studGroup = "КН-21-1";
+            studFaculty = "ФІКТ";
+            uniName = "Zhytomyr Polytechinc";
+        }
+        public override string ShowInfo()
+        {
+            return $"{firstName} {lastName}, {birthDate.dateToString()}. Вищий навчальний заклад - {uniName}, факультет {studFaculty}, {studCourse}-ий курс, група {studGroup}";
+        }
+    }
+    public class Teacher : Person {
+        public string Position { get; set; }
+        public string Chair { get; set; }
+        public string uniName { get; set; }
+        public Teacher(string firstName, string lastName, Date birthDate, string Position, string Chair, string uniName) : base(firstName, lastName, birthDate) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.birthDate = birthDate;
+            this.Position = Position;
+            this.Chair = Chair;
+            this.uniName = uniName;
+        }
+        public Teacher() {
+            firstName = "Олексій";
+            lastName = "Чижмотря";
+            birthDate = new Date(21, 07, 1983);
+            Position = "викладач практичних занять, голова приймальної комісії";
+            Chair = "121 Інженерія програмного забезпечення";
+            uniName = "Житомирська політехніка";
+        }
+        public override string ShowInfo() {
+            return $"{firstName} {lastName} {birthDate.dateToString()}. ВНЗ - {uniName}, кафедра - {Chair}, посада - {Position}";
+        }
+    }
+    public class LibraryUser : Person {
+        public int ticketNum { get; set; }
+        public Date issuanceDay { get; set; }
+        public int monthContrib { get; set; }
+        public LibraryUser(string firstName, string lastName, Date birthDate, int ticketNum, Date issuanceDay, int monthContrib) : base(firstName, lastName, birthDate) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.birthDate = birthDate;
+            this.ticketNum = ticketNum;
+            this.issuanceDay = issuanceDay;
+            this.monthContrib = monthContrib;
+        }
+        public LibraryUser() {
+            firstName = "Оксана";
+            lastName = "Перебийніс";
+            birthDate = new Date(9, 2, 2000);
+            ticketNum = 25814;
+            issuanceDay = new Date(10, 10, 2019);
+            monthContrib = 500;
+        }
+        public override string ShowInfo() {
+            return $"{firstName} {lastName} {birthDate.dateToString()}. Номер читацького квитка - {ticketNum}, дата видачі - {issuanceDay.dateToString()}, щомісячний внесок - {monthContrib} грн.";
         }
     }
     public class ExamResults
@@ -80,7 +162,6 @@
         }
         protected int math;
         public string thirdExamName { get; set; }
-        protected string thirdname;
         public int thirdExamRes {
             get {
                 return third;
@@ -92,7 +173,6 @@
         }
         protected int third;
         public string fourthExamName { get; set; }
-        protected string fourthname;
         public int fourthExamRes {
             get {
                 return fourth;
--- SanaCSharp06/ConsoleApp1/Program.cs
+++ SanaCSharp06/ConsoleApp1/Program.cs
@@ -7,13 +7,19 @@ internal class Program
     {
         Console.InputEncoding = Encoding.Unicode;
         Console.OutputEncoding = Encoding.Unicode;
-        List<Person> persons = new List<Person>();
-        persons.Add(new Person());
-        persons.Add(new Abiturient());
-        persons.Add(new Abiturient("Дмитро", "Стефанчук", new Date(8, 12, 2004), new ExamResults(181, 185, "Хімія", 178, "Історія України", 190), 11.1, "Радомишльська гімназія №2"));
+        List<Person> persons = new List<Person>
+        {
+            new Person(),
+            new Abiturient(),
+            new Abiturient("Дмитро", "Стефанчук", new Date(8, 12, 2004), new ExamResults(181, 185, "Хімія", 178, "Історія України", 190), 11.1, "Радомишльська гімназія №2"),
+            new Student(),
+            new Teacher("Ірина", "Погойдаш", new Date(5, 7, 1989), "Житомирська Політехніка", "121 Інженерія програмного забезпечення", "лектор"),
+            new Teacher(),
+            new LibraryUser()
+        };
         foreach (Person person in persons)
         {
-            Console.WriteLine(person.ShowInfo());
+            Console.WriteLine($"{person.ShowInfo()}\n");
         }
     }
 }
