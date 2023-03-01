16c4d588b5cbbe3c056a589840bd747ebd86fcd8
Eugune Biletskyi <biletskijzena@protonmail.com>
2023-03-01 14:38:35 +0200

    Completed SanaCSharp06

---
+++ SanaCSharp06/ClassLibrary/Human.cs
@@ -0,0 +1,26 @@
+﻿namespace ClassLibrary
+{
+    public class Human
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public Human(string name, string lastName, DateTime dateOfBirth)
+        {
+            Name = name;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+        }
+        public Human()
+        {
+            Name = "Human name";
+            LastName = "Human lastname";
+            DateOfBirth = DateTime.Now;
+        }
+        public virtual void ShowInfo()
+        {
+            Console.WriteLine($"Name: {Name}\nLastname: {LastName}\nDate of birthday: {DateOfBirth.ToShortDateString()}");
+        }
+    }
+}
