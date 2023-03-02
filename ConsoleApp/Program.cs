d8cbe2593f94182ab9b134b7b4215b5d42b63389
Valerii Piaskivskiy <sana22_pvo1@student.ztu.edu.ua>
2023-03-02 14:02:14 +0200

    Task 6 is done

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLib;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = UTF8Encoding.UTF8;
+            Teacher teacher = new Teacher("Тарас", "Василевич", "Декан", "ППВР", "ЖДУ");
+            teacher.ShowInfo();
+            Console.WriteLine();
+            LibraryUser LibUser = new LibraryUser("Петро", "Петренко", 55433, "01.03.2023", 250);
+            LibUser.ShowInfo();
+            Console.WriteLine();
+            Student student = new Student("Валерій", "П'ясківський","10.05.2002", 4, "РКТТ");
+            student.ShowInfo();
+            Console.WriteLine();
+            Person person = new Person("Валерій","П'ясківський","10.05.2002");
+            person.ShowInfo();
+            Console.WriteLine();
+            Entrant entrant = new Entrant("Валерій","П'ясківський", 10, "НСЗШ");
+            entrant.ShowInfo();
+        }
+    }
+}
+
