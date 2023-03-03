b6741f27f9839d09e468fdc3cbf8251e10fbb0a0
Москалик Олександр <xxteax@gmail.com>
2023-03-03 17:03:55 +0200

    Prakt08

---
+++ SanaCSharp06/ClassLibrary1/Teacher.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Teacher : Human
+    {
+        public string Position { get; set; }
+        public string Department { get; set; }
+        public string HigherEducation { get; set; }
+
+        public Teacher(string position, string department, string higherEducation, string name, string secondName) : base(name, secondName)
+        {
+            Position = position;
+            Department = department;
+            HigherEducation = higherEducation;
+        }
+
+        public Teacher(string position, string department, string name, string secondName) : base(name, secondName)
+        {
+            Position = position;
+            Department = department;
+        }
+
+        public override void ShowInfo()
+        {
+            base.ShowInfo();
+            Console.WriteLine($"Position: {Position}");
+            Console.WriteLine($"Department: {Department}");
+            Console.WriteLine($"Higher Education: {HigherEducation}");
+        }
+    }
+}
---
+++ SanaCSharp06/SanaCSharp06/Program.cs
@@ -0,0 +1,24 @@
+﻿using ClassLibrary1;
+
+namespace SanaCSharp06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Human ivan = new Human("Ivan", "Ivanov");
+            Human ivanna = new Human("Ivanna", "Ivannovna");
+            Applicant applicantIvan = new Applicant(200, 7.5f, ivan.Name, ivan.SecondName);
+            Applicant applicantIvanna = new Applicant(180, 9, ivanna.Name, ivanna.SecondName);
+            Student studentIvanna = new Student(4, "IPZ", "FIKT", "№33", applicantIvanna.ZnoScore, 
+                applicantIvanna.EducationScore, ivanna.Name, ivanna.SecondName);
+            Teacher anna = new Teacher("Programming Teacher", "Computer Science", "Anna", "Annovna");
+            LibraryUser ivannaUser = new LibraryUser(1231254125, new DateTime(2023, 3, 20), ivanna.Name, ivanna.SecondName,
+                studentIvanna.Course, studentIvanna.Group, studentIvanna.Faculty, studentIvanna.HigherEducation, studentIvanna.ZnoScore, studentIvanna.EducationScore);
+            applicantIvan.ShowInfo();
+            anna.ShowInfo();
+            ivannaUser.ShowInfo();
+
+        }
+    }
+}
