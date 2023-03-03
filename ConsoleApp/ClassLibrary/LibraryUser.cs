5eea98ac956038b01f7a02df593a293368b3d982
Sergiy Marchuk <sana22_msv@student.ztu.edu.ua>
2023-03-03 11:06:30 +0200

    task completed

---
+++ ConsoleApp/ClassLibrary/LibraryUser.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLib
+{
+    public class LibraryUser : Person
+    {
+        int ReaderTicketNumber { get; set; }
+        string DateOfIssue { get; set; }
+        decimal ContributionAmount { get; set; }
+        public LibraryUser(string firstName, string lastName,  string birthDate, 
+            int readerTicketNumber, string dateOfIssue, decimal contributionAmount) : base (firstName, lastName, birthDate)
+        {
+            ReaderTicketNumber = readerTicketNumber;
+            DateOfIssue = dateOfIssue;
+            ContributionAmount = contributionAmount;
+        }
+
+        public LibraryUser()
+        {
+
+        }
+
+        public LibraryUser(string firstName, string lastName, 
+            int readerTicketNumber, decimal contributionAmount) : base (firstName, lastName)
+        {
+            ReaderTicketNumber = readerTicketNumber;
+            ContributionAmount = contributionAmount;
+        }
+
+        public override void ShowInfo()
+        {
+            base.ShowInfo();
+            Console.WriteLine($"ReaderTicketNumber: {ReaderTicketNumber}\n" +
+                $"DateOfIssue: {DateOfIssue}\n" +
+                $"ContributionAmount: {ContributionAmount}\n");
+        }
+    }
+}
---
+++ ConsoleApp/ClassLibrary/Student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLib
+{
+    public class Student : Person
+    {
+        int Course { get; set; }
+        string Group { get; set; }
+        string Faculty { get; set; }
+        string University { get; set; }
+        public Student(string firstName, string lastName, string birthDate,
+            int course, string group, string faculty, string university) : base(firstName, lastName, birthDate)
+        {
+            Course = course;
+            Group = group;
+            Faculty = faculty;
+            University = university;
+        }
+
+        public Student()
+        {
+             
+        }
+
+        public Student(string firstName, string lastName, int course, string group, string faculty) :
+            base(firstName,lastName)
+        {
+            Course = course;
+            Group = group;
+            Faculty = faculty;
+        }
+
+        public override void ShowInfo()
+        {
+            base.ShowInfo();
+            Console.WriteLine($"Course: {Course}\n" +
+                $"Group: {Group}\n" +
+                $"Faculty: {Faculty}\n" +
+                $"University: {University}");
+        }
+
+
+    }
+}
