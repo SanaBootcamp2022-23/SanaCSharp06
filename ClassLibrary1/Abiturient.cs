d6404e3c430bc00684b14b515d258b692045b58c
Oleksandr Hanchevskyi <ipz222_goo@student.ztu.edu.ua>
2023-03-07 16:00:49 +0300

    all done

---
+++ ClassLibrary1/Abiturient.cs
@@ -0,0 +1,38 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using DocuSign.eSign.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Abiturient : Person
+    {
+        public int ZNO { get; set; }
+        public int Atestat { get; set; }
+        public string SchoolName { get; set; }
+        public Abiturient(string name, string lastName, Date birthDate, 
+            int zNO, int atestat, string schoolName) : base(name, lastName, birthDate)
+        {
+            ZNO = zNO;
+            Atestat = atestat;
+            SchoolName = schoolName;
+        }
+        public Abiturient() : base()
+        {
+            ZNO = 0;
+            Atestat = 0;
+            SchoolName = "None";
+        }
+        public override void ShowInfo()
+        {
+            base.ShowInfo();
+            Console.WriteLine(
+               $"Резульат ЗНО: {ZNO}\n" +
+               $"Бал атестата: {Atestat}\n" +
+               $"Назва школи: {SchoolName}\n");
+        }
+    }
+}
---
+++ SanaCSharp06/Program.cs
@@ -0,0 +1,20 @@
+﻿using ClassLibrary1;
+using System;
+
+
+class Program
+{
+   static void Main(string[] args)
+   {
+        Person person = new Person("Ганчевський","Олександр",new Date(2002,03,11));
+        person.ShowInfo();
+        Student student = new Student();
+        student.ShowInfo();
+        Abiturient abiturient = new Abiturient("Олександр","Ганчевський",new Date(2000,05,8),
+            200,11,"22");
+        abiturient.ShowInfo();  
+
+   }
+       
+}
+    
